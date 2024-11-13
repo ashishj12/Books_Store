2379159f8b4e8379e6682a6c02828804f6652bf3
ashishj12 <jangidashish38@gmail.com>
2024-11-13 19:24:21 +0530

    fix the cors issue

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173/","https://books-store-alpha.vercel.app/"],
+    origin: ["http://localhost:5173", "https://books-store-alpha.vercel.app"],
     credentials: true,
   })
 );
